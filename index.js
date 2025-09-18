6f9ce0a6f09439e87e893e1d88d87b8dc7481c81
Ivanuxus <119295581+Ivanuxus@users.noreply.github.com>
2025-09-18 14:18:24 +0300

    Added rooms, health potions, swords

--- index.js
+++ index.js
@@ -19,17 +19,18 @@ class Game {
     this.fieldHeight = parseInt($(".field").css("height"));
 
     let playerExist = false;
-    //Заливаем стенами
+
+    //Placing walls everywhere
     for (let j = 0; j < 40; j += 1) {
       for (let i = 0; i < 24; i += 1) {
         this.gameBoard[j][i] = 6;
-        // console.log(this.gameBoard[j][i] + " ");
       }
     }
 
-    //генерируем вертикальные и горизонтальные проходы
     let countOfVert = Math.floor(Math.random() * 3) + 3;
     let countOfHorizont = Math.floor(Math.random() * 3) + 3;
+
+    //Generating Horizontal Tunnels
     let counter = 0;
     const filled = [];
     while (counter < countOfHorizont) {
@@ -49,9 +50,10 @@ class Game {
       let ran = filled[i];
       for (let j = 0; j < 40; j += 1) {
         this.gameBoard[j][ran] = 1;
-        // console.log(this.gameBoard[j][i] + " ");
       }
     }
+
+    //Generating Vertical Tunnels
     counter = 0;
     filled.length = 0;
     while (counter < countOfVert) {
@@ -71,10 +73,10 @@ class Game {
       let ran = filled[i];
       for (let j = 0; j < 24; j += 1) {
         this.gameBoard[ran][j] = 1;
-        // console.log(this.gameBoard[j][i] + " ");
       }
     }
 
+    //Placing a player in random place
     for (let j = 0; j < 40; j += 1) {
       for (let i = 0; i < 24; i += 1) {
         if (
@@ -88,7 +90,82 @@ class Game {
         }
       }
     }
+
+    //Placing 10 enemies in random place
+    let counterOfEnemies = 0;
+    const coordXOfEnemies = [10];
+    const coordYOfEnemies = [10];
+    for (let j = 0; j < 40; j += 1) {
+      for (let i = 0; i < 24; i += 1) {
+        if (
+          Math.floor(Math.random() * 25) == 2 &&
+          counterOfEnemies < 10 &&
+          this.gameBoard[j][i] == 1
+        ) {
+          this.gameBoard[j][i] = 2;
+          coordXOfEnemies[counterOfEnemies] = j;
+          coordYOfEnemies[counterOfEnemies] = i;
+          counterOfEnemies += 1;
+        }
+      }
+    }
+    //placing 10 HP in radom place
+    let counterOfHP = 0;
+    const coordXOfHP = [10];
+    const coordYOfHP = [10];
+    for (let j = 0; j < 40; j += 1) {
+      for (let i = 0; i < 24; i += 1) {
+        if (
+          Math.floor(Math.random() * 25) == 2 &&
+          counterOfHP < 10 &&
+          this.gameBoard[j][i] == 1
+        ) {
+          this.gameBoard[j][i] = 3;
+          coordXOfHP[counterOfHP] = j;
+          coordYOfHP[counterOfHP] = i;
+          counterOfHP += 1;
+        }
+      }
+    }
+    //placing 2 sw in radom place
+    let counterOfSw = 0;
+    const coordXOfSw = [10];
+    const coordYOfSw = [10];
+    for (let j = 0; j < 40; j += 1) {
+      for (let i = 0; i < 24; i += 1) {
+        if (
+          Math.floor(Math.random() * 25) == 2 &&
+          counterOfSw < 2 &&
+          this.gameBoard[j][i] == 1
+        ) {
+          this.gameBoard[j][i] = 5;
+          coordXOfSw[counterOfSw] = j;
+          coordYOfSw[counterOfSw] = i;
+          counterOfSw += 1;
+        }
+      }
+    }
+    //placing &numberOfRooms in random place
+    let numberOfRooms = Math.floor(Math.random() * 5) + 5;
+    let counterOfRooms = 0;
+    while (counterOfRooms < numberOfRooms) {
+      let widthOfRoom = Math.floor(Math.random() * 5) + 3;
+      let heigthOfRoom = Math.floor(Math.random() * 5) + 3;
+      let XOfRoom = Math.floor(Math.random() * 32);
+      let YOfRoom = Math.floor(Math.random() * 16);
+      for (let x = XOfRoom; x < XOfRoom + widthOfRoom; x += 1) {
+        for (let y = YOfRoom; y < YOfRoom + heigthOfRoom; y += 1) {
+          if (true) {
+            this.gameBoard[x][y] = 1;
+          }
+        }
+      }
+      counterOfRooms++;
+    }
+
+    this.redraw();
   }
+  //Meaning the number in matrice
   //tile- 1
   //tile-E 2
   //ttile-HP 3
@@ -148,6 +225,7 @@ class Game {
     $(".field").empty();
     this.draw();
   }
+
   move() {
     let player = $(".field .tileP");
     let movePlayer = 10;
@@ -156,7 +234,9 @@ class Game {
         case "a":
           if (
             this.playerj - 1 >= 0 &&
-            this.gameBoard[this.playerj - 1][this.playeri] == 1
+            (this.gameBoard[this.playerj - 1][this.playeri] == 1 ||
+              this.gameBoard[this.playerj - 1][this.playeri] == 3 ||
+              this.gameBoard[this.playerj - 1][this.playeri] == 5)
           ) {
             this.playerj -= 1;
             this.gameBoard[this.playerj][this.playeri] = 1;
@@ -165,7 +245,9 @@ class Game {
         case "d":
           if (
             this.playerj + 1 < 40 &&
-            this.gameBoard[this.playerj + 1][this.playeri] == 1
+            (this.gameBoard[this.playerj + 1][this.playeri] == 1 ||
+              this.gameBoard[this.playerj + 1][this.playeri] == 3 ||
+              this.gameBoard[this.playerj + 1][this.playeri] == 5)
           ) {
             this.playerj += 1;
             this.gameBoard[this.playerj][this.playeri] = 1;
@@ -174,7 +256,9 @@ class Game {
         case "w":
           if (
             this.playeri - 1 >= 0 &&
-            this.gameBoard[this.playerj][this.playeri - 1] == 1
+            (this.gameBoard[this.playerj][this.playeri - 1] == 1 ||
+              this.gameBoard[this.playerj][this.playeri - 1] == 3 ||
+              this.gameBoard[this.playerj][this.playeri - 1] == 5)
           ) {
             this.playeri -= 1;
             this.gameBoard[this.playerj][this.playeri] = 1;
@@ -183,7 +267,9 @@ class Game {
         case "s":
           if (
             this.playeri + 1 < 24 &&
-            this.gameBoard[this.playerj][this.playeri + 1] == 1
+            (this.gameBoard[this.playerj][this.playeri + 1] == 1 ||
+              this.gameBoard[this.playerj][this.playeri + 1] == 3 ||
+              this.gameBoard[this.playerj][this.playeri + 1] == 5)
           ) {
             this.playeri += 1;
             this.gameBoard[this.playerj][this.playeri] = 1;
